25fb2c8cdc4a3405eaaf0535e6cf3ec83028d9be
Jorge Menezes <jorgedimendes@hotmail.com>
2023-06-04 11:35:18 -0300

    Adicionado variaveis

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -2,6 +2,7 @@ package co.tiagoaguiar.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,8 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Buscar os objetos e ter a referencia deles
         val editText: EditText = findViewById(R.id.editnumber)
         val txtResult: TextView = findViewById(R.id.textresult)
         val buttonGenerate: Button = findViewById(R.id.button_generateNumbers)
+
+        // Opção 01: XML
+        // Opção 02: Variavel que seja do tipo wiew.onClickListener (interface)
+
+        // Opção 03: Mais simples possivel- Bloco de codigo que será disparado pelo onClickListener
+        buttonGenerate.setOnClickListener {
+            Log.i("Teste", "Botão clicado!!!")
+
+        }
+
     }
+
+
+
 }
