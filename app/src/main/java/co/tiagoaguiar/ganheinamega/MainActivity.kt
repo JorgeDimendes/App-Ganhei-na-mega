1d8f262dd9f3f05cf6567889a652bf05357b33e9
Jorge <jorgedimendes@hotmail.com>
2023-06-04 05:31:59 -0300

    Alterações Layout

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -2,10 +2,17 @@ package co.tiagoaguiar.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.editnumber)
+        val txtResult: TextView = findViewById(R.id.textresult)
+        val buttonGenerate: Button = findViewById(R.id.button_generateNumbers)
     }
 }
