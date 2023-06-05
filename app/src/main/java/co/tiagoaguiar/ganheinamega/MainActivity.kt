3c27b1bce3ff5c3ea4fa15afcd5f64e6f2df3420
Jorge Menezes <jorgedimendes@hotmail.com>
2023-06-05 10:25:23 -0300

    Re-começando projeto, deu um erro e achei melhor iniciar do zero.

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -3,71 +3,104 @@ package co.tiagoaguiar.ganheinamega
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // aqui onde voce decide o que o app fazer...
         setContentView(R.layout.activity_main)
 
-        //Buscar os objetos e ter a referencia deles
-        val editText: EditText = findViewById(R.id.editnumber)
-        val txtResult: TextView = findViewById(R.id.textresult)
-        val buttonGenerate: Button = findViewById(R.id.button_generateNumbers)
+        // buscar os objetos e ter a referencia deles
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
 
-        // Opção 01: XML
-        // Opção 02: Variavel que seja do tipo wiew.onClickListener (interface)
+        // opção 1: XML
 
-        // Opção 03: Mais simples possivel- Bloco de codigo que será disparado pelo onClickListener
-        buttonGenerate.setOnClickListener {
+        // opção 2: variavel que seja do tipo (objeto anonimo) View.OnClickListener (interface)
+        // btnGenerate.setOnClickListener(buttonClickListener)
+
+        // opção 3: mais simples possivel - bloco de código que será disparado pelo onClickListener
+        btnGenerate.setOnClickListener {
+            // aqui podemos colocar nossa logica de programação. porque será disparado depois do
+            // evento de touch do usuário
 
             val text = editText.text.toString()
 
             numberGenerator(text, txtResult)
-            txtResult.text = text
         }
-
     }
 
-    private fun numberGenerator(text: String, txtResult: TextView) =
-        //Validar quando o campo é vazio
-        if (text.isNotEmpty()){
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        // validar quando o campo é vazio
+        if (text.isNotEmpty()) {
 
-            val quantidade = text.toInt() //Converte String para inteiro
+            val qtd = text.toInt() // convert string para inteiro
 
-            if (quantidade >=6 && quantidade <= 15){
+            // validar se o campo informado é entre 6 e 15
+            if (qtd >= 6 && qtd <= 15) {
 
                 val numbers = mutableSetOf<Int>()
                 val random = Random()
 
-                while(true){
-                    val number = random.nextInt(60) // 0..59
+                while(true) {
+                    val number = random.nextInt(60) // 0...59
                     numbers.add(number + 1)
 
-                    if (numbers.size == quantidade){
+                    if (numbers.size == qtd) {
                         break
                     }
                 }
 
-                txtResult.text = numbers.joinToString { " - " }
+                txtResult.text = numbers.joinToString(" - ")
 
-
-            }else{
-                //Caso o cliente não digite nada mostra esse alerta
-                Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
             }
 
-        } else{
-            //Caso o cliente não digite nada mostra esse alerta
-            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+
+        } else {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
         }
 
 
-    // Validar se o compo informado é entre 6 e 15
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    val buttonClickListener = View.OnClickListener {
+//        Log.i("Teste", "botao clicado!!!")
+//    }
+
+//    val buttonClickListener = object : View.OnClickListener {
+//        // quem chama o onclick é o proprio SDK do android que dispara apos o evento de touch
+//        override fun onClick(v: View?) {
+//            Log.i("Teste", "botao clicado!!!")
+//        }
+//
+//    }
+
+    // opção 1: XML
+//    fun buttonClicked(view: View) {
+//        Log.i("Teste", "botao clicado!!!")
+//    }
 
 }
