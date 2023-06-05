0b878f7f476962791cad5c5c8c272e5406c1d9fa
Jorge <jorgedimendes@hotmail.com>
2023-06-05 04:13:39 -0300

    Feito alterações de soma

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +34,40 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun numberGenerator(text: String, txtResult: TextView){
+    private fun numberGenerator(text: String, txtResult: TextView) =
+        //Validar quando o campo é vazio
+        if (text.isNotEmpty()){
 
-    }
+            val quantidade = text.toInt() //Converte String para inteiro
+
+            if (quantidade >=6 && quantidade <= 15){
+
+                val numbers = mutableSetOf<Int>()
+                val random = Random()
+
+                while(true){
+                    val number = random.nextInt(60) // 0..59
+                    numbers.add(number + 1)
+
+                    if (numbers.size == quantidade){
+                        break
+                    }
+                }
+
+                txtResult.text = numbers.joinToString { " - " }
+
+
+            }else{
+                //Caso o cliente não digite nada mostra esse alerta
+                Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            }
+
+        } else{
+            //Caso o cliente não digite nada mostra esse alerta
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+        }
+
+
+    // Validar se o compo informado é entre 6 e 15
 
 }
