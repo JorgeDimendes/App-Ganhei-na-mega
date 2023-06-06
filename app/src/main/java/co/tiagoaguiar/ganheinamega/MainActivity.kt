4c7e3151f747fcae6e6b22022f3d908ba462f406
Jorge Menezes <jorgedimendes@hotmail.com>
2023-06-06 10:37:26 -0300

    Finalizado Projeto

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +33,35 @@ class MainActivity : AppCompatActivity() {
         // Validar quando o campo é vazio
         if(text.isNotEmpty()){
 
+            val qtd = text.toInt() // Converter sting para inteiro
+
+            // Validar se o campo informado é entre 6 e 15
+            if(qtd >= 6 && qtd <= 15){
+
+                val numbers = mutableSetOf<Int>()
+                val random = Random()
+
+                while (true){
+                    val number = random.nextInt(60) // 0...59
+                    numbers.add(number + 1)
+
+                    if(numbers.size == qtd){
+                        break
+                    }
+                }
+
+                txtResult.text = numbers.joinToString(" - ")
+
+
+            }else{
+                Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            }
+
+        } else{
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
         }
 
-        // Validar se o campo informado é entre 6 e 15
+
 
     }
 
