05b94ec29cfde40eddc53547d0e6ed30628caea0
Jorge Menezes <jorgedimendes@hotmail.com>
2023-06-06 09:55:00 -0300

    Adicionado função numberGenerator()

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -2,10 +2,40 @@ package co.tiagoaguiar.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        // Buscar os objetos e ter a referencia deles
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        btnGenerate.setOnClickListener {
+
+            val text = editText.text.toString()
+
+            numberGenerator(text, txtResult)
+        }
+
     }
+
+    private fun numberGenerator(text: String, txtResult: TextView){
+        // Validar quando o campo é vazio
+        if(text.isNotEmpty()){
+
+        }
+
+        // Validar se o campo informado é entre 6 e 15
+
+    }
+
+
 }
